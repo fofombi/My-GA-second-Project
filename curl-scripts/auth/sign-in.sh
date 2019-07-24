b0442027ad814b332d3113643d32890775deed79
fofombi <fofombi@gmail.com>
2019-07-24 16:44:36 -0400

    Today's commit

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -3,7 +3,7 @@
 
 # BAhJIiU2ODgyYmZiMzI0OGVjNGQyZDY0NzI1NTQxODE0NzUyZgY6BkVG--4b587249db80de9732b7b8f0e0fe626bb2b5308b
 
-curl "https://wdi-library-api.herokuapp.com/sign-in" \
+curl "http://localhost:4741/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
